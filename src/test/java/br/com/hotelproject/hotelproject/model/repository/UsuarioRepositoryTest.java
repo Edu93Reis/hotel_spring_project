ddc59055871aa15c6fc123fdb4bb0730464867b9
edureis.s <eduardo.reis93@gmail.com>
2021-05-27 20:59:08 -0300

    Atualizando configuração pom para evitar erro surefire - mvn install

--- src/test/java/br/com/hotelproject/hotelproject/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/hotelproject/hotelproject/model/repository/UsuarioRepositoryTest.java
@@ -4,22 +4,33 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.hotelproject.hotelproject.model.entity.Usuario;
 
-@SpringBootTest
-@RunWith( SpringRunner.class)
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")//busca application-"test"//corrigir config
+@DataJpaTest//usa commmit e rollback
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class UsuarioRepositoryTest {
 	
 	@Autowired
 	public UsuarioRepository usuarioRepository;
 	
+	@Autowired
+	TestEntityManager entityManager;
+	
 	@Test
 	public void testaValidacaoEmail() {
 		
 		Usuario usuario = new Usuario("user@test.com","user");
+		
+		entityManager.persist(usuario);
 		
 		boolean valida = usuarioRepository.existsByEmail(usuario.getEmail());
 		
@@ -27,23 +38,23 @@ public class UsuarioRepositoryTest {
 		
 	}
 	
-//	@Test
-//	public void testaCadastroUsuario() {
-//		
-//		Usuario usuario = new Usuario("John Doe", "user@test.com", "user", 120);
-//		
-//		Usuario teste = usuarioRepository.save(usuario);
-//		
-//		Assertions.assertThat(teste).asList();
-//		
-//	}
-//	
+	@Test
+	public void testaCadastroUsuario() {
+		
+		Usuario usuario = new Usuario("John Doe", "user@test.com", "user", 120);
+		
+		Usuario teste = usuarioRepository.save(usuario);
+		
+		Assertions.assertThat(teste.getIdUsuario()).isNotNull();
+		
+	}
+	
 //	@Test
 //	public void testaAutenticacaoUsuario() {
 //		
 //		Usuario usuario = new Usuario("user@test.com", "user");
 //		
-//		Usuario teste = usuarioRepository.save(usuario);
+//		Usuario teste = entityManager.persist(usuario);
 //		
 //		Assertions.assertThat(teste).asList();
 //		
--- src/test/java/br/com/hotelproject/hotelproject/service/UsuarioServiceTest.java
+++ src/test/java/br/com/hotelproject/hotelproject/service/UsuarioServiceTest.java
@@ -1,10 +1,14 @@
 package br.com.hotelproject.hotelproject.service;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import br.com.hotelproject.hotelproject.exceptions.AutenticacaoException;
+import br.com.hotelproject.hotelproject.exceptions.EmailJaCadastradoException;
+import br.com.hotelproject.hotelproject.model.entity.Usuario;
 import br.com.hotelproject.hotelproject.model.repository.UsuarioRepository;
 import br.com.hotelproject.hotelproject.model.service.UsuarioService;
 
@@ -18,6 +22,49 @@ public class UsuarioServiceTest {
 	@Autowired
 	UsuarioRepository usuarioRepository;
 	
+	@Test(expected = Test.None.class)
+	public void testaValidaEmail() {
+		
+		usuarioRepository.deleteAll();
+		
+		usuarioService.validaEmail("email@email.com");
+		
+	}
 	
+	@Test(expected = EmailJaCadastradoException.class)
+	public void testaEmailCadastrado() {
+		
+		Usuario usuario = new Usuario("John Doe", "email@email.com", "000", 12);
+		
+		usuarioRepository.save(usuario);
+		
+		usuarioService.validaEmail("email@email.com");
+		
+	}
+	
+	@Test(expected = Test.None.class)
+	public void testaCadastraUsuario() {
+		
+		Usuario usuario = new Usuario("John Doe", "email@email.com", "000", 12);
+		
+		usuarioService.cadastraUsuario(usuario);
+	
+		
+		
+	}
+	
+	@Test(expected = Test.None.class)
+	public void testaValidaUsuarioValido() {
+	
+		usuarioService.autenticarUsuario("email@email.com", "000");
+		
+	}
+	
+	@Test(expected = AutenticacaoException.class)
+	public void testaValidaUsuarioInvalido() {
+		
+		usuarioService.autenticarUsuario("email1@email.com", "001");
+		
+	}
 	
 }
