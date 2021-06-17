a67831dfbab524507ff91617d865e9c79404d702
edureis.s <eduardo.reis93@gmail.com>
2021-06-17 10:46:41 -0300

    montando testes de serviço

--- src/test/java/br/com/hotelproject/hotelproject/service/UsuarioServiceTest.java
+++ src/test/java/br/com/hotelproject/hotelproject/service/UsuarioServiceTest.java
@@ -1,10 +1,12 @@
 package br.com.hotelproject.hotelproject.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,18 +15,39 @@ import br.com.hotelproject.hotelproject.exceptions.EmailJaCadastradoException;
 import br.com.hotelproject.hotelproject.model.entity.Usuario;
 import br.com.hotelproject.hotelproject.model.repository.UsuarioRepository;
 import br.com.hotelproject.hotelproject.model.service.UsuarioService;
+import br.com.hotelproject.hotelproject.model.service.impl.UsuarioServiceImpl;
 
 @SpringBootTest
 @ActiveProfiles("test")//busca application-"test"//corrigir config
-@DataJpaTest//usa commmit e rollback
+//@DataJpaTest//usa commmit e rollback
 @RunWith(SpringRunner.class)
 public class UsuarioServiceTest {
 
 	@Autowired
 	UsuarioService usuarioService;
 	
-	@Autowired
+//	@Autowired
+	@MockBean
 	UsuarioRepository usuarioRepository;
+	
+	@Before
+	public void setUp() {
+		usuarioService = new UsuarioServiceImpl(usuarioRepository);
+	}
+	
+	@Test( expected = Test.None.class )
+	public void deveAutenticarUsuarioComSucesso() {
+		
+		String email = "teste@teste.com";
+		String senha = "123";
+		
+		Usuario usuario = criaUsuario();
+		
+	}
+	
+	public Usuario criaUsuario() {
+		return new Usuario("John Wick","teste@teste.com","123",32);
+	}
 	
 	@Test(expected = Test.None.class)
 	public void testaValidaEmail() {
